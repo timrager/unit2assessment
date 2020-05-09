da39d6c56526ff412ae5e70a0c7725163f556c13
timrager <tim.rager@gmail.com>
2020-05-09 13:58:15 -0700

    heroku

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const Todo = require('./models/todos.js');
+const db = mongoose.connection;
 const PORT = process.env.port || 3000;
 // const methodOverride = require('method-override');
 
@@ -10,6 +11,7 @@ const PORT = process.env.port || 3000;
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 app.set('view engine', 'jsx');
+app.use(express.json());
 app.engine('jsx', require('express-react-views').createEngine());
 // app.use(methodOverride('_method'));
 
@@ -17,10 +19,12 @@ app.engine('jsx', require('express-react-views').createEngine());
 // mongoose connection
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/unit2assessment';
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.connection.once('open', () => {
-    console.log("connected to mongo");
-})
+db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
+db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
+db.on('disconnected', () => console.log('mongo disconnected'));
 
+// open the connection to mongo
+db.on('open' , ()=>{});
 
 // index route
 app.get('/', (req, res) => {
